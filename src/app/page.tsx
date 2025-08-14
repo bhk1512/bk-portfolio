ef41ac374d8c526965d41dd6b84eb82021c36bac
bhk1512 <bharat.15dck@gmail.com>
2025-08-14 19:45:36 +0530

    avatar bigger

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -563,7 +563,8 @@ function ProfileAvatar({
     <div
       ref={ref}
       className="group relative rounded-full overflow-hidden ring-1 ring-zinc-800 transition-transform duration-300 will-change-transform hover:scale-[1.04]
-                 w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32 lg:w-56 lg:h-56 xl:w-64 xl:h-64"
+                 w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32 
+                 lg:w-88 lg:h-88 xl:w-96 xl:h-96"
       style={{ perspective: 600 }}
     >
       {/* soft halo */}
